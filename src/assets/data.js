f4bbac396250a70900214086a8f762699357655c
EniolaAdemola <it.eniolaademola>
2023-01-07 10:34:46 +0100

    add data file

---
+++ src/assets/data.js
@@ -0,0 +1,23 @@
+export const aboutStory = [
+	{
+		id: 1,
+		story: "December 2022, a group of computing science studentDecember 2022, a group of computing science studentDecember 2022, a group of computing science studentDecember 2022, a group of computing science student"
+	},
+	{
+		id: 2,
+		story: "December 2022, a group of computing science studentDecember 2022, a group of computing science studentDecember 2022, a group of computing science studentDecember 2022, a group of computing science student"
+	},
+	{
+		id: 3,
+		story: "December 2022, a group of computing science studentDecember 2022, a group of computing science studentDecember 2022, a group of computing science studentDecember 2022, a group of computing science student"
+	},
+	{
+		id: 4,
+		story: "December 2022, a group of computing science studentDecember 2022, a group of computing science studentDecember 2022, a group of computing science studentDecember 2022, a group of computing science student"
+	},
+	{
+		id: 5,
+		story: "December 2022, a group of computing science studentDecember 2022, a group of computing science studentDecember 2022, a group of computing science studentDecember 2022, a group of computing science student"
+	},
+	
+];
