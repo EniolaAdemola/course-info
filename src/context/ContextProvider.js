4d67099366af610b957416c822f52a77a51506f1
EniolaAdemola <it.eniolaademola>
2023-01-09 17:19:38 +0100

    add context provider

---
+++ src/context/ContextProvider.js
@@ -0,0 +1,39 @@
+import React, { createContext, useContext, useState } from "react";
+
+const StepInfoContext = createContext();
+
+const ContextProvider = ({ children }) => {
+	const [activeStyle, setActiveStyle] = useState(1);
+
+    const [userDetails, setUserDetails] = useState({
+        userName: "",
+        courseOfStudy: "Computer Science",
+        level: ""
+    })
+
+    const handleChange = (event) => {
+		const { name, value } = event.target;
+
+		setUserDetails((prevValue) => {
+			return {
+				...prevValue,
+				[name]: value,
+			};
+		});
+	};
+
+	return (
+		<StepInfoContext.Provider
+			value={{ userDetails, activeStyle, setActiveStyle, userDetails, setUserDetails, handleChange }}
+		>
+			{children}
+		</StepInfoContext.Provider>
+	);
+};
+
+export default ContextProvider;
+
+// return the Register context
+export const useStepInfoContext = () => {
+	return useContext(StepInfoContext);
+};
