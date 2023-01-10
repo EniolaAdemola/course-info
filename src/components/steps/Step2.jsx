d543652c715aadb22356b235ba019d31e3c6e592
EniolaAdemola <it.eniolaademola>
2023-01-10 19:50:32 +0100

    add step2 component

---
+++ src/components/steps/Step2.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useStepInfoContext } from '../../context/ContextProvider';
+
+const Step2 = () => {
+  const { activeStyle, setActiveStyle, userDetails, handleChange } = useStepInfoContext();
+  
+
+  return (
+    <div className='courseofstudy'>
+    <h2 htmlFor="department">Hey {userDetails.userName}, kindly choose your course of study:</h2>
+    {/* Course of study is automatically Computer science from the context provider */}
+    <select name="courseOfStudy" id="department" onChange={handleChange}>
+      <option value="N/A">N/A</option>
+      <option value="Computer Science" required>Computer science</option>
+      {/* <option value="Moore Science" required>More science</option> */}
+    </select>
+    {userDetails.courseOfStudy !== "" && <button
+					type="submit"
+          onClick={()=> setActiveStyle(activeStyle+1)}
+				>
+					Next
+          
+		</button>}
+    </div>
+  )
+}
+
+export default Step2
