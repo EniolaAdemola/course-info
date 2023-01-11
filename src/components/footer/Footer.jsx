1413ec7d0fa846c6075ddf084104f4683162b3df
EniolaAdemola <it.eniolaademola>
2023-01-11 21:23:28 +0100

    add fotter component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,54 @@
-import React from 'react'
-import "./footer.scss"
+import React from "react";
+import { Link } from "react-router-dom";
+import "./footer.scss";
+import { socialIcons, terms } from "../../assets/data";
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+	return (
+		<footer>
+			<div className="container">
+				<hr />
+				<div className="footer__container">
+					<Link to="/" className="logo">
+						<h1>
+							COU<span>RSE</span>INFO
+						</h1>
+					</Link>
+					<ul className="terms">
+						{terms.map(({ id, text,path }) => {
+							return (
+                <Link key={id} to={path}>
+                  <li>{text}</li>
+                </Link>
+              );
+						})}
+					</ul>
+					<div className="socials__container">
+						<p className="mail">hello@courseinfo.co</p>
+						<div className="footer__socials">
+							{socialIcons.map(({ id, icon, link }) => {
+								return (
+									<a
+										key={id}
+										href={link}
+										target="_blank"
+										rel="noreferrer noopener"
+									>
+										{icon}
+									</a>
+								);
+							})}
+						</div>
+						<p>&copy; 22/23 (CSC401 - Software Development) <br/> Group 3. All Rights Reserved</p>
+					</div>
+					<p className="footer__end">
+						Over <span>120,000+ Students</span> have checked
+						their course information on our website
+					</p>
+				</div>
+			</div>
+		</footer>
+	);
+};
 
-export default Footer
+export default Footer;
