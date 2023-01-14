434bbe6e545d82b7cfe5c9eb86861e71c37b00fb
EniolaAdemola <it.eniolaademola>
2023-01-14 06:55:44 +0100

    add course details

---
+++ src/assets/couresDetails.js
@@ -0,0 +1,726 @@
+export const coursesDetails = [
+    {
+		id: 1,
+		courseCode: "CIT 111  ",
+		courseTitle: " Introduction to Information & Communication Technology ",
+		lecturer: " Dr I.K Ogundoyin ",
+		unit: " 2 ",
+		courseDay: "  ",
+		courseTime:"  "
+	 },
+	 
+	 {
+		id: 2 ,
+		courseCode: " BIO 101  ",
+		courseTitle: "Basic Biology I  ",
+		lecturer: " Dr Adeleke ",
+		unit: " 3  ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 3 ,
+		courseCode: " BIO 107 ",
+		courseTitle: " Experimental Biology ",
+		lecturer: " **** ",
+		unit: " 1 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 4 ,
+		courseCode: "CHM 101  ",
+		courseTitle: " General Chemistry ",
+		lecturer: " Dr Awojide ",
+		unit: " 3 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 5,
+		courseCode: "CHM 107  ",
+		courseTitle: " Experimental Chemistry ",
+		lecturer: " **** ",
+		unit: " 1 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 6,
+		courseCode: " PHY 101 ",
+		courseTitle: " General Physics  ",
+		lecturer: " Dr Alabi ",
+		unit: " 3 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 7,
+		courseCode: " MTH 101 ",
+		courseTitle: " General Mathematics ",
+		lecturer: " Dr Kolawole ",
+		unit: " 3 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 8,
+		courseCode: " GNS 101 ",
+		courseTitle: "Use of English  ",
+		lecturer: " Dr Olatunji ",
+		unit: " 2 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 9 ,
+		courseCode: " LIB 101  ",
+		courseTitle: " Use of Library ",
+		lecturer: "  ",
+		unit: " 2 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 10,
+		courseCode: " FRN 221 ",
+		courseTitle: " Basic French ",
+		lecturer: " Dr Wahab ",
+		unit: "2  ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 	 
+	 {
+		id: 11,
+		courseCode: " CIT 112 ",
+		courseTitle: " Introduction to Computer Programming ",
+		lecturer: " Miss Dimple Ogunbiyi ",
+		unit: " 2 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 12,
+		courseCode: "BIO 102  ",
+		courseTitle: "Basic Biology II  ",
+		lecturer: " Dr Alabi ",
+		unit: " 3 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 13,
+		courseCode: "BIO 108  ",
+		courseTitle: " Experimental Biology II ",
+		lecturer: " **** ",
+		unit: "1  ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 14 ,
+		courseCode: " CHM 102 ",
+		courseTitle: " General Chemistry II ",
+		lecturer: " Dr Anifowoshe ",
+		unit: " 3 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 15,
+		courseCode: " CHM 108 ",
+		courseTitle: " Experimental Chemistry  ",
+		lecturer: " **** ",
+		unit: "  1",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 16,
+		courseCode: " PHY 102 ",
+		courseTitle: " General Physics  ",
+		lecturer: " Dr Fakunle ",
+		unit: " 3 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 17,
+		courseCode: " PHY 108 ",
+		courseTitle: " Experimental Physics ",
+		lecturer: " **** ",
+		unit: "1  ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 18 ,
+		courseCode: " MTH102  ",
+		courseTitle: " General Mathematics II ",
+		lecturer: " Dr Kolawole ",
+		unit: "3  ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 {
+		id:19 ,
+		courseCode: " GNS 102 ",
+		courseTitle: " Use of English II ",
+		lecturer: " Dr Olatunji ",
+		unit: " 2 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id:20 ,
+		courseCode: " FRN 222 ",
+		courseTitle: " French for Specific Purpose ",
+		lecturer: " Dr Wahab ",
+		unit: " 2 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 	 
+	 {
+		id:21 ,
+		courseCode: " CIT 201 ",
+		courseTitle: " Structured Programming ",
+		lecturer: " Dr I.K Ogundoyin ",
+		unit: " 3  ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 22,
+		courseCode: " CIT 203 ",
+		courseTitle: "Computer Achitecture and Organization I  ",
+		lecturer: " Dr L. Omotosho ",
+		unit: " 2 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id:23 ,
+		courseCode: " CIT 205 ",
+		courseTitle: " Computer Hardware ",
+		lecturer: " Dr K.O Jimoh ",
+		unit: " 2 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 24,
+		courseCode: " CIT 207  ",
+		courseTitle: " Introduction to Computer Network ",
+		lecturer: " Dr Ishola ",
+		unit: " 2  ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 25,
+		courseCode: " CIT 291  ",
+		courseTitle: " Computer Workshop Practice ",
+		lecturer: "  ",
+		unit: " 1 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id:26 ,
+		courseCode: " STA 201 ",
+		courseTitle: " Statistics for Phtsical Science & Engineering ",
+		lecturer: " Dr Ojurongbe ",
+		unit: " 4 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id:27 ,
+		courseCode: " GNS 201 ",
+		courseTitle: " Nigerian People and Culture ",
+		lecturer: " **** ",
+		unit: " 2 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 28,
+		courseCode: " MTH 201  ",
+		courseTitle: " Mathematical Methods I ",
+		lecturer: " Dr Kolawole ",
+		unit: " 2 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id:29 ,
+		courseCode: "CIT 209  ",
+		courseTitle: "Data Management I  ",
+		lecturer: " Dr A.A Adigun ",
+		unit: "2  ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 30,
+		courseCode: " MTH 203 ",
+		courseTitle: " Linear Algebra I ",
+		lecturer: " Dr Kolawole ",
+		unit: " 2 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 	 
+	 {
+		id:31 ,
+		courseCode: " CIT 202 ",
+		courseTitle: " Low Level Language ",
+		lecturer: " Dr Y.O Adebayo ",
+		unit: " 3 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id:32 ,
+		courseCode: " CIT 204 ",
+		courseTitle: " Compiler Construction I  ",
+		lecturer: " Dr Ishola ",
+		unit: "2  ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id:33 ,
+		courseCode: " CIT 206 ",
+		courseTitle: " Principles of Operating System I ",
+		lecturer: " Dr A.A Adigun ",
+		unit: " 2 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id:34 ,
+		courseCode: " CIT 208 ",
+		courseTitle: " Fundamental of Data Structure ",
+		lecturer: " Dr K.O Jimoh ",
+		unit: " 3 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 35,
+		courseCode: " CIT 292  ",
+		courseTitle: " Computer Workshop Practice II ",
+		lecturer: " **** ",
+		unit: " 1 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id:36 ,
+		courseCode: " PHY 206 ",
+		courseTitle: " Electric Circuit and Electronics ",
+		lecturer: " Dr Olatona ",
+		unit: " 3 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 37,
+		courseCode: " GNS 202 ",
+		courseTitle: " Osun Peoples and Culture ",
+		lecturer: " **** ",
+		unit: " 2 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 38,
+		courseCode: " GNS 302 ",
+		courseTitle: " Introduction to Logic and Phylosophy  ",
+		lecturer: " **** ",
+		unit: " 2 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id:39 ,
+		courseCode: " MTH 204 ",
+		courseTitle: " Linear Algebra II  ",
+		lecturer: "  ",
+		unit: " 2 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	//  {
+	// 	id:	40 ,
+	// 	courseCode: "  ",
+	// 	courseTitle: "  ",
+	// 	lecturer: "  ",
+	// 	unit: "  ",
+	// 	courseDay: "  ",
+	// 	courseTime:"  ",
+	//  },
+	 
+	 {
+		id: 41,
+		courseCode: " CIT 210 ",
+		courseTitle: " Computational Science and Numerical Methods ",
+		lecturer: " Prof Odejobi Ajadi Odetunji ",
+		unit: " 3 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	//  300 Level Harmattan Semester
+	 
+	 {
+		id: 42,
+		courseCode: " CIT 301  ",
+		courseTitle: " Object Oriented Programming ",
+		lecturer: " Miss Dimple ",
+		unit: " 3 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 43,
+		courseCode: " CIT 303  ",
+		courseTitle: " Algorithm and Complexity Analysis ",
+		lecturer: " Dr Ishola ",
+		unit: " 3 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 44,
+		courseCode: " CIT 305 ",
+		courseTitle: " Principal of Operating System II ",
+		lecturer: " Dr A.A Adigun ",
+		unit: "  3",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 45 ,
+		courseCode: " CIT 307 ",
+		courseTitle: " System Analysis and Design Methodologies  ",
+		lecturer: " Dr L. Omotosho ",
+		unit: " 3 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 46 ,
+		courseCode: " CIT 311 ",
+		courseTitle: " Computer Architecture and Organization II ",
+		lecturer: " Dr L. Omotosho ",
+		unit: " 2 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 47,
+		courseCode: " CIT 391 ",
+		courseTitle: "Digital Computer Laboratory  ",
+		lecturer: " **** ",
+		unit: " 1 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 47.5,
+		courseCode: " GNS 301 ",
+		courseTitle: " Entrepreneur Skill, Development and Practice ",
+		lecturer: " **** ",
+		unit: " 2 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 48 ,
+		courseCode: "CIT 309  ",
+		courseTitle: " Survey of Programming Languages  ",
+		lecturer: " Dr Ibrahim ",
+		unit: " 3 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 49,
+		courseCode: " CIT 313 ",
+		courseTitle: " Operation Research  ",
+		lecturer: " Dr Patrick Ozoh ",
+		unit: " 3  ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 50 ,
+		courseCode: " MTH 205 ",
+		courseTitle: " Sets , Logic and Algebra ",
+		lecturer: " Dr Kolawole ",
+		unit: "  ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	//  300 Level Raining Semester
+	 
+	 {
+		id: 51,
+		courseCode: " CIT 399  ",
+		courseTitle: " Industrial Attachment ",
+		lecturer: " **** ",
+		unit: " 16 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	//  400 Level Harmttan Semester
+	 
+	 {
+		id: 52,
+		courseCode: " CIT 401 ",
+		courseTitle: " Software Engineering ",
+		lecturer: " Prof Akanbi ",
+		unit: " 4 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 53,
+		courseCode: " CIT 403 ",
+		courseTitle: " Artificial Intelligence  ",
+		lecturer: " Dr Ibrahim ",
+		unit: " 3 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 54,
+		courseCode: " CIT 405 ",
+		courseTitle: " Net-Centric Computing ",
+		lecturer: " Dr K.O Jimoh ",
+		unit: " 3 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 55,
+		courseCode: " CIT 497 ",
+		courseTitle: " Research Methodology/Seminar ",
+		lecturer: " Prof Odejobi Ajadi Odetunji ",
+		unit: " 2 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 56,
+		courseCode: " CIT 407 ",
+		courseTitle: " DIgital Computer Network and Communication  ",
+		lecturer: " Dr Y.O Adebayo ",
+		unit: "  ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 57,
+		courseCode: " CIT 409 ",
+		courseTitle: " Computer Graphics and Visualisation ",
+		lecturer: " Dr A.A Adigun ",
+		unit: " 3 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 58,
+		courseCode: "CIT 411  ",
+		courseTitle: "Information Technology Policy and Security  ",
+		lecturer: " Dr L. Omotosho ",
+		unit: " 3 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 59,
+		courseCode: " CIT 413 ",
+		courseTitle: " Special Topics in Information Technology ",
+		lecturer: " Dr Patrick Ozoh ",
+		unit: " 3 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 60,
+		courseCode: " CIT 415 ",
+		courseTitle: " Distributed Computing System ",
+		lecturer: " Dr K.O Jimoh ",
+		unit: " 3 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 61,
+		courseCode: " CIT 417 ",
+		courseTitle: " Agent Based System ",
+		lecturer: " Prof Akanbi ",
+		unit: " 3 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },	 
+	 
+	 {
+		id: 62,
+		courseCode: " CIT 402 ",
+		courseTitle: " Data Management II ",
+		lecturer: " Dr A.A Adigun ",
+		unit: " 3 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 63,
+		courseCode: " CIT 404  ",
+		courseTitle: " Human Computer Interface ",
+		lecturer: "  ",
+		unit: " 2 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 64 ,
+		courseCode: " CIT 406 ",
+		courseTitle: "modeling and Simulation  ",
+		lecturer: "  ",
+		unit: " 3 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 65,
+		courseCode: "CIT 499  ",
+		courseTitle: " Research Project ",
+		lecturer: "  ",
+		unit: " 4 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 66,
+		courseCode: " CIT 408 ",
+		courseTitle: " Compiler Construction II ",
+		lecturer: "  ",
+		unit: " 3 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 67,
+		courseCode: " CIT 410 ",
+		courseTitle: " Project Management ",
+		lecturer: "  ",
+		unit: " 3 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 68,
+		courseCode: " CIT 412 ",
+		courseTitle: "Queuing Systems  ",
+		lecturer: "  ",
+		unit: " 3 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 69,
+		courseCode: " CIT 414 ",
+		courseTitle: " Computer System Performance Evaluation ",
+		lecturer: "  ",
+		unit: " 3 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 70,
+		courseCode: " CIT 416 ",
+		courseTitle: " Formal Models of Computaton ",
+		lecturer: "  ",
+		unit: "3  ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+	 
+	 {
+		id: 71,
+		courseCode: " CIT 418 ",
+		courseTitle: " Computer Based Distance Learning ",
+		lecturer: "  ",
+		unit: " 3 ",
+		courseDay: "  ",
+		courseTime:"  ",
+	 },
+  ]
