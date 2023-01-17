e99ddc9ecb6a28736135698c6cbd8453826256ec
EniolaAdemola <it.eniolaademola>
2023-01-17 13:28:30 +0100

    update assets data

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,3 +1,8 @@
+import { AiOutlineTwitter } from "react-icons/ai";
+import { AiOutlineGithub } from "react-icons/ai";
+import { FaFacebookF } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+
 export const aboutStory = [
 	{
 		id: 1,
@@ -10,14 +15,83 @@ export const aboutStory = [
 	{
 		id: 3,
 		story: "December 2022, a group of computing science studentDecember 2022, a group of computing science studentDecember 2022, a group of computing science studentDecember 2022, a group of computing science student"
+	}	
+];
+
+export const levels = [
+    {
+      id: 1,
+      userLevel: "100L"
+    },
+    {
+      id: 2,
+      userLevel: "200L"
+    },
+    {
+      id: 3,
+      userLevel: "300L"
+    },
+    {
+      id: 4,
+      userLevel: "400L"
+    }
+  ];
+
+  export const determineLink = [
+	{
+		id: 1,
+		path: "spend",
+		text: "Check my course info",
+	},
+	{
+		id: 2,
+		path: "purchase",
+		text: "Check a particular course info",
+	},
+];
+
+export const terms = [
+	{
+		id: 1,
+    path:"/check-info",
+		text: "Check my course info",
+	},
+	{
+    id: 2,
+    path:"/search-info",
+		text: "Search a   Course Info",
+	},
+	{
+    id: 3,
+    path:"/developers",
+		text: "Developers",
+	},
+	{
+    id: 4,
+    path:"/about",
+		text: "About",
+	},
+];
+
+export const socialIcons = [
+	{
+		id: 1,
+		icon: <AiOutlineTwitter />,
+		link: "https://twitter.com/_daveworld",
+	},
+	{
+		id: 2,
+		icon: <AiOutlineGithub />,
+		link: "https://www.github.com/EniolaAdemola/",
+	},
+	{
+		id: 3,
+		icon: <FaFacebookF />,
+		link: "https://web.facebook.com/eniola.ademola.1610/",
 	},
 	{
 		id: 4,
-		story: "December 2022, a group of computing science studentDecember 2022, a group of computing science studentDecember 2022, a group of computing science studentDecember 2022, a group of computing science student"
+		icon: <FaLinkedin />,
+		link: "https://www.linkedin.com/in/eniola-ademola",
 	},
-	{
-		id: 5,
-		story: "December 2022, a group of computing science studentDecember 2022, a group of computing science studentDecember 2022, a group of computing science studentDecember 2022, a group of computing science student"
-	},
-	
 ];
