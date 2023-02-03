5e315575bf1d400435414e1137a0c0b15baa5465
EniolaADemola <it.eniolaademola@gmail.com>
2023-02-03 06:37:58 -0800

    update infosection

--- src/components/InfoSection.jsx
+++ src/components/InfoSection.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import {RiArrowRightSLine} from "react-icons/ri"
 
 const InfoSection = () => {
   return (
@@ -7,10 +8,12 @@ const InfoSection = () => {
       <h1 className=''>Check my course information <div className="headline"></div></h1>
       <div className='info__section-container'>
          <Link className='box' to="/check-info">
-
+            <h3>Check my course info</h3>
+            <RiArrowRightSLine className='arrow'/>
          </Link>
          <Link className='box' to="/search-info">
-
+            <h3>Search a course info</h3>
+            <RiArrowRightSLine className='arrow'/>
          </Link>
       </div>
     </section>
