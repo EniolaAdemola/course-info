45fab96fa76b0d84a124a70899b53ed7a76e35a6
EniolaADemola <it.eniolaademola@gmail.com>
2023-02-03 05:35:10 -0800

    add mainheader component

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Image from "../assets/images/image.png"
 
 const MainHeader = () => {
   return (
-    <div>MainHeader</div>
+    <header className="main__header">
+			<div className="main__header-container">
+				<div
+					className="main__header-left"
+				>
+					<h1>Worried about your course information ?</h1>
+					<p>
+						COURSEINFO provides you with information about the course you are offering in university.
+					</p>
+					<Link to="/check-info" className="btn lg">
+						Check My COURSEINFO - it's free
+					</Link>
+				</div>
+				<div className="main__header-right">
+					<div
+						className="main__header-image"
+					>
+						<img src={Image} alt="MainHeaderImage" />
+					</div>
+				</div>
+			</div>
+		</header>
   )
 }
 
