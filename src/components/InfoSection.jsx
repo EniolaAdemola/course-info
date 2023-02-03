96358574f4c7e3f5db01f2ce2ef945a3dfa77a85
EniolaADemola <it.eniolaademola@gmail.com>
2023-02-03 05:34:55 -0800

    add info section

--- src/components/InfoSection.jsx
+++ src/components/InfoSection.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const InfoSection = () => {
   return (
-    <div>InfoSection</div>
+    <section className='info__section'>
+      <h1 className=''>Check my course information <div className="headline"></div></h1>
+      <div className='info__section-container'>
+         <Link className='box' to="/check-info">
+
+         </Link>
+         <Link className='box' to="/search-info">
+
+         </Link>
+      </div>
+    </section>
   )
 }
 
